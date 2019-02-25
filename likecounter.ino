13b2efebe494990fe20c907852bc96cc88ec273c
ayu69 <Q.marques58@gmail.com>
2019-02-25 09:23:04 +0100

    Add files via upload

--- likecounter.ino
+++ likecounter.ino
@@ -1,7 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
+#include <NeoPixelBus.h>
 
+const uint16_t PixelCount = 4; // this example assumes 4 pixels, making it smaller will cause a failure
+const uint8_t PixelPin = 2;  // make sure to set this to the correct pin, ignored for Esp8266
+
+#define colorSaturation 128
+
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
+
+RgbColor red(colorSaturation, 0, 0);
+RgbColor black(0);
 
 
 #ifndef STASSID
@@ -48,6 +58,10 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+
+  strip.Begin();
+  strip.Show();
   
 }
 
@@ -77,8 +91,20 @@ void facebook() {
   JsonObject& root = jsonBuffer.parseObject(line);
   int FBLike = root[String("fan_count")];
 
+  int FBLike_Brut = FBLike;
+  int FB_unite = FBLike_Brut % 10;
+  int FB_dizaine = FBLike_Brut / 10 % 10;
+  int FB_centaine = FBLike_Brut / 100 % 10;
+  int FB_millier = FBLike_Brut / 1000 % 10;
+  int FB_dizaine_millier = FBLike_Brut / 10000 % 10;
+
   Serial.print("FB Like: ");
-  Serial.println(FBLike);
+  Serial.println(FBLike_Brut);
+  Serial.println(FB_unite);
+  Serial.println(FB_dizaine);
+  Serial.println(FB_centaine);
+  Serial.println(FB_millier);
+  Serial.println(FB_dizaine_millier);
 }
 
 void twitter() {
@@ -89,7 +115,7 @@ void twitter() {
     return;
   }
   
-  String url = "/widgets/followbutton/info.json?screen_names="+TW_Name;
+  String url = "/widgets/followbutton/info.json?screen_names=" + TW_Name;
 
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + TWhost + "\r\n" +
@@ -111,8 +137,20 @@ void twitter() {
   JsonObject& root = jsonBuffer.parseObject(line);
   int followers_count = root[String("followers_count")];
 
+  int TWLike_Brut = followers_count;
+  int TW_unite = TWLike_Brut % 10;
+  int TW_dizaine = TWLike_Brut / 10 % 10;
+  int TW_centaine = TWLike_Brut / 100 % 10;
+  int TW_millier = TWLike_Brut / 1000 % 10;
+  int TW_dizaine_millier = TWLike_Brut / 10000 % 10;
+
   Serial.print("Followers Twitter: ");
-  Serial.println(followers_count);
+  Serial.println(TWLike_Brut);
+  Serial.println(TW_unite);
+  Serial.println(TW_dizaine);
+  Serial.println(TW_centaine);
+  Serial.println(TW_millier);
+  Serial.println(TW_dizaine_millier);
 }
 
 void instagram() {
@@ -141,17 +179,31 @@ void instagram() {
   JsonObject& root = jsonBuffer.parseObject(line);
   
   JsonObject& Body_Data = root["data"];
-  int follower = Body_Data["counts"]["followed_by"];
+  int INfollower = Body_Data["counts"]["followed_by"];
 
-  Serial.print("Insta Fans: ");
-  Serial.println(follower);
+  int INLike_Brut = INfollower;
+  int IN_unite = INLike_Brut % 10;
+  int IN_dizaine = INLike_Brut / 10 % 10;
+  int IN_centaine = INLike_Brut / 100 % 10;
+  int IN_millier = INLike_Brut / 1000 % 10;
+  int IN_dizaine_millier = INLike_Brut / 10000 % 10;
+
+  Serial.print("Insta Fans:");
+  Serial.println(INLike_Brut);
+  Serial.println(IN_unite);
+  Serial.println(IN_dizaine);
+  Serial.println(IN_centaine);
+  Serial.println(IN_millier);
+  Serial.println(IN_dizaine_millier);
+
+
+
 }
 
 void loop() {
 
-  facebook();
-  twitter();
-  instagram();
-  delay(10000);
+  //facebook();
+  //twitter();
+  //instagram();
 
 }
